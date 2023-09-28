7635ad87dbffb5804fe2a0fe5781ab9c0d3331f9
Fernandez Santiago <santiago@mbpdefernandez.home>
2023-09-28 18:42:21 -0300

    Creation of url_paths

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from PyPDF2 import PdfFileReader
 from pdf2image import convert_from_path
 from PIL import Image
@@ -6,21 +7,50 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from datetime import datetime
 
-def convert_pdf_to_image():
-    for filename in os.listdir(pdf_folder):
-        if filename.endswith(".pdf"):
-            pdf_path = os.path.join(pdf_folder, filename)
+def convert_pdfs_to_images():
+    # Nombre del archivo que contiene las URL y rutas de los PDFs
+    url_path_file = "urls/urls_path.txt"
+    # Carpeta donde quieres guardar las capturas
+    snapshot_folder = "snapshots"
+    os.makedirs(snapshot_folder, exist_ok=True)
 
+    # Leer todas las líneas del archivo url_path_file
+    with open(url_path_file, "r") as file:
+        lines = file.readlines()
+
+    # Crear una lista para almacenar las líneas actualizadas
+    updated_lines = []
+
+    for line in lines:
+        line = line.strip()  # Elimina espacios y saltos de línea al principio y al final de la línea
+        pdf_url, pdf_path = line.split(" ; ")  # Divide la línea en URL y ruta del PDF
+        print(pdf_path)
+        # Comprueba si el archivo PDF existe antes de intentar convertirlo
+        if os.path.exists(pdf_path):
             # Convierte el PDF en una imagen (captura de la primera página)
             images = convert_from_path(pdf_path, first_page=0, last_page=1)
 
-            if images:
-                # Guarda la primera página como un archivo PNG en la carpeta de capturas
-                snapshot_path = os.path.join(snapshot_folder, f"{os.path.splitext(filename)[0]}.png")
-                images[0].save(snapshot_path, "PNG")
+        if images:
+            # Genera la ruta para la imagen PNG en la carpeta "snapshots"
+            image_path = os.path.join(snapshot_folder, os.path.basename(pdf_path).replace(".pdf", ".png"))
 
-                print(f"Captura guardada para {filename} como {snapshot_path}")
-    print("Todas las capturas se han creado exitosamente!")
+            # Guarda la primera página como un archivo PNG en la carpeta "snapshots"
+            images[0].save(image_path, "PNG")
+
+            # Agrega la ruta del snapshot a la línea actual
+            line = f"{pdf_url} ; {pdf_path} ; {image_path}\n"
+
+            print(f"Imagen guardada para {pdf_path} como {image_path}")
+        else:
+            print(f"El PDF {pdf_path} no existe.")
+
+        # Agregar la línea actualizada a la lista
+        updated_lines.append(line)
+
+    # Reemplazar el contenido del archivo "urls_path.txt" con las líneas actualizadas
+    with open(url_path_file, "w") as url_path:
+        url_path.writelines(updated_lines)
+        
 
 def combine_images_to_pdf():
     # Carpeta donde están las imágenes
@@ -54,6 +84,33 @@ def combine_images_to_pdf():
     
     print(f"Se ha creado el archivo PDF combinado como {output_pdf}")
 
+def download_pdfs_from_urls(txt_file):
+    # Ruta completa al archivo que contendrá las URL y rutas de los PDFs descargados
+    url_path_file = os.path.join("urls", "urls_path.txt")
+
+    with open(txt_file, "r") as file:
+        urls = file.readlines()
+
+    for url in urls:
+        url = url.strip()  # Elimina espacios y saltos de línea al principio y al final de la URL
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            # Obtiene el nombre del archivo desde la URL
+            filename = os.path.join(pdf_folder, os.path.basename(url))
+            
+            # Guarda el PDF en la carpeta "pdfs"
+            with open(filename, "wb") as pdf_file:
+                pdf_file.write(response.content)
+            
+            print(f"Se ha descargado y guardado el PDF desde {url} como {filename}")
+
+            # Agrega la URL y la ruta del PDF al archivo "url_path.txt" en la carpeta "urls"
+            with open(url_path_file, "a") as url_path:
+                url_path.write(f"{url} ; {filename}\n")
+        else:
+            print(f"No se pudo descargar el PDF desde {url}")
+
 # Carpeta que contiene los PDFs
 pdf_folder = "pdfs"
 os.makedirs(pdf_folder, exist_ok=True)
@@ -61,8 +118,6 @@ os.makedirs(pdf_folder, exist_ok=True)
 snapshot_folder = "snapshots"
 os.makedirs(snapshot_folder, exist_ok=True)
 
-# Itera a través de los archivos PDF en la carpeta
-convert_pdf_to_image()
-# Llama a la función para combinar las imágenes en un PDF
-combine_images_to_pdf()
-
+# Llama a la función y proporciona la ruta completa al archivo TXT en la carpeta "urls"
+download_pdfs_from_urls(os.path.join("urls", "urls.txt"))
+convert_pdfs_to_images()
